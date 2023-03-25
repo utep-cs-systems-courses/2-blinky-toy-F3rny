6c0ae403a3a4f5e76ca189a704c08c5c93984311
F3rny <fhremes@miners.utep.edu>
2023-03-25 13:29:01 -0600

    updating demo 11

--- demos/11-buzzer/buzzerMain.c
+++ demos/11-buzzer/buzzerMain.c
@@ -6,7 +6,7 @@ int main() {
     configureClocks();
  
     buzzer_init();
-    buzzer_set_period(1000);	/* start buzzing!!! 2MHz/1000 = 2kHz*/
+    buzzer_set_period(7644);	/* start buzzing!!! 2MHz/261.63 = 7.6kHz*/ //C4
 
 
     or_sr(0x18);          // CPU off, GIE on
